c284a1bdf73283d40949623a28efc45fcc54e10a
mizutismask <severinek@gmail.com>
2023-06-14 09:51:44 +0200

    feat: add ContextLog utilities

--- modules/APP_Extended.php
+++ modules/APP_Extended.php
@@ -397,6 +397,43 @@ abstract class APP_Extended extends Table {
         }
     }
 
+    function dbInsertContextLog($action, $param1 = null, $param2 = null, $param3 = null) {
+        $state = $this->gamestate->state()["name"];
+        $player = $this->getMostlyActivePlayerId();
+        $values[] = "( '$player', '$state', '$action', '$param1', '$param2', '$param3')";
+        $sql = "INSERT INTO context_log (player, state, action, param1, param2, param3)";
+        $sql .= " VALUES " . implode(",", $values);
+        $this->DbQuery($sql);
+    }
+
+    function dbResolveContextLog($contextId) {
+        if (!$contextId) {
+            $this->error("resolve context log can not be called with an undefined id");
+        } else {
+            $value = 1;
+            $sql = "UPDATE context_log SET resolved = '$value' WHERE id = $contextId";
+            self::DbQuery($sql);
+        }
+    }
+
+    function dbGetLastContextToResolve($count = 1) {
+        $positiveCount = $count <= 0 ? 1 : $count;
+        $sql = "select * from context_log where resolved = 0 order by id desc limit $positiveCount";
+        $res=self::getObjectListFromDB($sql);
+        if($count==1 && count($res)) return $res[0];
+        return $res;
+    }
+
+    function dbGetLastResolvedContext() {
+        $sql = "select * from context_log where resolved = 1 order by id desc limit 1";
+        $res=self::getObjectListFromDB($sql);
+        if(count($res)) return $res[0];
+        return $res;
+    }
+
+    function dbEmptyTable($tableName) {
+        self::DbQuery("delete from $tableName");
+    }
 
     /**
      * Changes values of multiactivity in db, does not sent notifications.
