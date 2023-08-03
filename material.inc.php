2eb019ebd13880228a0c3699e7af09ee0dfc2b76
mizutismask <severinek@gmail.com>
2023-08-03 16:04:08 +0200

    feat: add more 1x1 bonuses since they are unlimited

--- material.inc.php
+++ material.inc.php
@@ -792,7 +792,7 @@ $this->token_types = [
     'w' => 1,
     'h' => 1,
     'color' => 'bonus',
-    'occurrences' => 6,
+    'occurrences' => 16,
   ],
   'patch_26' => [
     'num' => 26,
@@ -992,7 +992,7 @@ $this->token_types = [
     'w' => 1,
     'h' => 1,
     'color' => 'bonus',
-    'occurrences' => 5,
+    'occurrences' => 15,
   ],
   'patch_46' => [
     'num' => 46,
@@ -1082,7 +1082,7 @@ $this->token_types = [
     'w' => 1,
     'h' => 1,
     'color' => 'bonus',
-    'occurrences' => 5,
+    'occurrences' => 15,
   ],
   'patch_55' => [
     'num' => 55,
