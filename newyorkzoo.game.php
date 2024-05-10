27e8be43e449d035dfed3e45b0270f261846c6a3
mizutismask <severinek@gmail.com>
2024-05-10 16:08:21 +0200

    refacto: integrated loadBugReportSQL

--- newyorkzoo.game.php
+++ newyorkzoo.game.php
@@ -1837,6 +1837,7 @@ class NewYorkZoo extends EuroGame {
     function arg_possibleMovesByPatch($patches, $player_id, $bonuses = false) {
         $res = [];
         $playerOrder = $this->getPlayerPosition($player_id);
+       // self::dump('*************arg_placeAttraction***patches before***', $patches);
         if ($bonuses)
             $patches = $this->getUniqueMasks($patches);
         //self::dump('*************arg_placeAttraction***patches***', $patches);
@@ -2630,44 +2631,17 @@ class NewYorkZoo extends EuroGame {
     }
 
     /*
-   * loadBug: in studio, type loadBug(20762) into the table chat to load a bug report from production
-   * client side JavaScript will fetch each URL below in sequence, then refresh the page
-   */
-    public function loadBug($reportId) {
-        $db = explode('_', self::getUniqueValueFromDB("SELECT SUBSTRING_INDEX(DATABASE(), '_', -2)"));
-        $game = $db[0];
-        $tableId = $db[1];
-        self::notifyAllPlayers('loadBug', "Trying to load <a href='https://boardgamearena.com/bug?id=$reportId' target='_blank'>bug report $reportId</a>", [
-            'urls' => [
-                // Emulates "load bug report" in control panel
-                "https://studio.boardgamearena.com/admin/studio/getSavedGameStateFromProduction.html?game=$game&report_id=$reportId&table_id=$tableId",
-
-                // Emulates "load 1" at this table
-                "https://studio.boardgamearena.com/table/table/loadSaveState.html?table=$tableId&state=1",
-
-                // Calls the function below to update SQL
-                "https://studio.boardgamearena.com/1/$game/$game/loadBugSQL.html?table=$tableId&report_id=$reportId",
-
-                // Emulates "clear PHP cache" in control panel
-                // Needed at the end because BGA is caching player info
-                "https://studio.boardgamearena.com/admin/studio/clearGameserverPhpCache.html?game=$game",
-            ]
-        ]);
-    }
-
-    /*
-    * loadBugSQL: in studio, this is one of the URLs triggered by loadBug() above
+    * New method to load a game from a report and change player ids
     */
-    public function loadBugSQL($reportId) {
-        $studioPlayer = self::getCurrentPlayerId();
-        $players = self::getObjectListFromDb("SELECT player_id FROM player", true);
-
+    public function loadBugReportSQL(int $reportId, array $studioPlayersIds): void {
+        $players = $this->getObjectListFromDb('SELECT player_id FROM player', true);
+  
         // Change for your game
         // We are setting the current state to match the start of a player's turn if it's already game over
-        $sql = [
-            "UPDATE global SET global_value=2 WHERE global_id=1 AND global_value=99"
-        ];
-        foreach ($players as $pId) {
+        $sql = ['UPDATE global SET global_value=10 WHERE global_id=1 AND global_value=99'];
+        foreach ($players as $index => $pId) {
+          $studioPlayer = $studioPlayersIds[$index];
+    
             // All games can keep this SQL
             $sql[] = "UPDATE player SET player_id=$studioPlayer WHERE player_id=$pId";
             $sql[] = "UPDATE global SET global_value=$studioPlayer WHERE global_value=$pId";
@@ -2678,19 +2652,11 @@ class NewYorkZoo extends EuroGame {
 
             // TODO Add game-specific SQL updates for the tables, everywhere players ids are used in your game 
             $sql[] = "UPDATE context_log SET player=$studioPlayer WHERE player=$pId";
-
-            // This could be improved, it assumes you had sequential studio accounts before loading
-            // e.g., quietmint0, quietmint1, quietmint2, etc. are at the table
-            $studioPlayer++;
         }
-        $msg = "<b>Loaded <a href='https://boardgamearena.com/bug?id=$reportId' target='_blank'>bug report $reportId</a></b><hr><ul><li>" . implode(';</li><li>', $sql) . ';</li></ul>';
-        self::warn($msg);
-        self::notifyAllPlayers('message', $msg, []);
 
         foreach ($sql as $q) {
             self::DbQuery($q);
         }
         self::reloadPlayersBasicInfos();
-        $this->gamestate->reloadState();
     }
 }
